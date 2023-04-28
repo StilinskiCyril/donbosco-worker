70cdc74c54bb94e602098eccb0431349db54613b
Cyril Aguvasu <aguvasucyril@gmail.com>
2023-04-28 14:19:11 +0300

    user model configuration

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,35 +4,44 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasPermissions;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, HasRoles;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles, HasPermissions, SoftDeletes;
+
+    protected static function boot()
+    {
+        parent::boot();
+        self::creating(function ($uuid){
+            $uuid->uuid = Str::uuid()->toString();
+        });
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'uuid';
+    }
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $guarded = ['id'];
 
     /**
      * The attributes that should be hidden for serialization.
      *
      * @var array<int, string>
      */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    protected $hidden = ['id', 'password','remember_token', 'deleted_at', 'updated_at'];
 
     /**
      * The attributes that should be cast.
@@ -42,4 +51,31 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function scopeFilter($q){
+        if (!is_null(request('uuid')) && !empty(request('uuid'))) {
+            $q->where('uuid', request('uuid'));
+        }
+        if (!is_null(request('name')) && !empty(request('name'))) {
+            $q->where('name', request('name'));
+        }
+        if (!is_null(request('msisdn')) && !empty(request('msisdn'))) {
+            $q->where('msisdn', request('msisdn'));
+        }
+        if (!is_null(request('email')) && !empty(request('email'))) {
+            $q->where('email', request('email'));
+        }
+        if (!is_null(request('sort_by')) && !empty(request('sort_by'))) {
+            if (request('sort_by') == 'random') {
+                $q->inRandomOrder();
+            }
+            if (request('sort_by') == 'latest') {
+                $q->latest();
+            }
+            if (request('sort_by') == 'oldest') {
+                $q->oldest();
+            }
+        }
+        return $q;
+    }
 }
