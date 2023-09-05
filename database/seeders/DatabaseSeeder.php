c8aab2b572b8f961a847adad915f77e92c7697f3
Cyril Aguvasu <aguvasucyril@gmail.com>
2023-09-05 10:58:45 +0300

    updated seeds

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,6 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        $this->call(PermissionsSeeder::class);
         $this->call(RolesSeeder::class);
         $this->call(UsersSeeder::class);
     }
--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -15,60 +15,18 @@ class UsersSeeder extends Seeder
      */
     public function run(): void
     {
-        $super_admin = User::where('msisdn', '254705799644')->first();
-        if(!$super_admin){
-            $user_super_admin = User::create([
-                'name' => 'Cyril Super Admin',
+        $admin = User::where('msisdn', '254705799644')->first();
+        if(!$admin){
+            $admin = User::create([
+                'name' => 'Cyril Aguvasu',
                 'msisdn' => '254705799644',
                 'msisdn_verified_at' => now(),
-                'email' => 'aguvasucyrilsuperadmin@gmail.com',
+                'email' => 'aguvasucyril@gmail.com',
                 'email_verified_at' => now(),
-                'password' => Hash::make('aguvasucyrilsuperadmin@gmail.com'),
+                'password' => Hash::make('aguvasucyril@gmail.com'),
                 'remember_token' => Str::random(10),
             ]);
-            $user_super_admin->assignRole('super-admin');
-        }
-
-        $admin = User::where('msisdn', '254705799645')->first();
-        if(!$admin){
-            $user_admin = User::create([
-                'name' => 'Cyril Admin',
-                'msisdn' => '254705799645',
-                'msisdn_verified_at' => now(),
-                'email' => 'aguvasucyriladmin@gmail.com',
-                'email_verified_at' => now(),
-                'password' => Hash::make('aguvasucyriladmin@gmail.com'),
-                'remember_token' => Str::random(10),
-            ]);
-            $user_admin->assignRole('admin');
-        }
-
-//        $treasurer = User::where('msisdn', '254705799646')->first();
-//        if(!$treasurer){
-//            $user_treasurer = User::create([
-//                'name' => 'Cyril Admin',
-//                'msisdn' => '254705799646',
-//                'msisdn_verified_at' => now(),
-//                'email' => 'aguvasucyriltreasurer@gmail.com',
-//                'email_verified_at' => now(),
-//                'password' => Hash::make('aguvasucyriltreasurer@gmail.com'),
-//                'remember_token' => Str::random(10),
-//            ]);
-//            $user_treasurer->assignRole('treasurer');
-//        }
-
-        $user = User::where('msisdn', '254705799647')->first();
-        if(!$user){
-            $user_user = User::create([
-                'name' => 'Cyril Admin',
-                'msisdn' => '254705799647',
-                'msisdn_verified_at' => now(),
-                'email' => 'aguvasucyriluser@gmail.com',
-                'email_verified_at' => now(),
-                'password' => Hash::make('aguvasucyriluser@gmail.com'),
-                'remember_token' => Str::random(10),
-            ]);
-            $user_user->assignRole('user');
+            $admin->assignRole('admin');
         }
     }
 }
